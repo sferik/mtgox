c212470e0cb0360a963fcca5836c96bb2be563c7
T S <YenTheFirst@gmail.com>
2011-06-16 12:46:18 -0700

    remove stray comment

--- lib/mtgox.rb
+++ lib/mtgox.rb
@@ -3,7 +3,7 @@ require 'mtgox/client'
 module MtGox
   ORDER_TYPES = {:buy=>2, :sell=>1}
   STATUS_TYPES = {:active=>1, :not_enough_funds=>2}
-  #class configure
+  
   class << self
   	attr_accessor :name, :pass
   	def configure
