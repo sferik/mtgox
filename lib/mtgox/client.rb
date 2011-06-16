dea1b7948c7cd99d5eb71a1b4bfac8eca621b541
Erik Michaels-Ober <sferik@gmail.com>
2011-06-16 15:36:59 -0700

    Fix specs on Ruby 1.8.7 and lower

--- lib/mtgox/client.rb
+++ lib/mtgox/client.rb
@@ -17,18 +17,18 @@ module MtGox
 
     # Fetch open asks
     #
-    # @return [Array<Array<Numeric>>] returns an array of arrays of numbers. these are of the form [price,quantity]. The array is sorted in price ascending order
+    # @return [Array<Array<Numeric>>] returns an array of arrays of numbers. these are of the form [price, quantity]. The array is sorted in price ascending order
     # @example
-    #   MtGox.asks[0,3] #=> [[19.3898, 3.9], [19.4, 48.264], [19.409, 1]]
+    #   MtGox.asks[0, 3] #=> [[19.3898, 3.9], [19.4, 48.264], [19.409, 1]]
     def asks
       get('/code/data/getDepth.php')['asks']
     end
 
     # Fetch open bids
     #
-    # @return [Array<Array<Numeric>>] returns an array of arrays of numbers. these are of the form [price,quantity]. the array is sorted in price descending order
+    # @return [Array<Array<Numeric>>] returns an array of arrays of numbers. these are of the form [price, quantity]. the array is sorted in price descending order
     # @example
-    #   MtGox.bids[0,3] #=> [[19.3898, 77.42], [19.3, 3.02], [19.29, 82.378]]
+    #   MtGox.bids[0, 3] #=> [[19.3898, 77.42], [19.3, 3.02], [19.29, 82.378]]
     def bids
       get('/code/data/getDepth.php')['bids']
     end
@@ -38,22 +38,23 @@ module MtGox
     # @return [Hashie::Rash] a hash with keys "asks" and "buys", which contain arrays as described in #asks and #bids
     # @example
     #   o=MtGox.offers;
-    #   o.asks[0,3] #=> [[19.3898, 3.9], [19.4, 48.264], [19.409, 1]]
-    #   o.bids[0,3] #=> [[19.3898, 77.42], [19.3, 3.02], [19.29, 82.378]]
+    #   o.asks[0, 3] #=> [[19.3898, 3.9], [19.4, 48.264], [19.409, 1]]
+    #   o.bids[0, 3] #=> [[19.3898, 77.42], [19.3, 3.02], [19.29, 82.378]]
     def offers
       get('/code/data/getDepth.php')
     end
-
 
     # Fetch recent trades
     #
     # @return [Array<Hashie::Rash>] an array of trades, sorted in chronological order. each trade is a Hashie::Rash, with keys :amount - number of bitcoins traded, :price - price they were traded at, in USD, :date - time and date of the trade (ruby Time object), :tid - Trade ID
     # @example
-    #   MtGox.trades[0,3] #=> [<#Hashie::Rash amount=41 date=2011-06-14 11:26:32 -0700 price=18.5 tid="183747">, <#Hashie::Rash amount=5 date=2011-06-14 11:26:44 -0700 price=18.5 tid="183748">, <#Hashie::Rash amount=5 date=2011-06-14 11:27:00 -0700 price=18.42 tid="183749">]
+    #   MtGox.trades[0, 3] #=> [<#Hashie::Rash amount=41 date=2011-06-14 11:26:32 -0700 price=18.5 tid="183747">, <#Hashie::Rash amount=5 date=2011-06-14 11:26:44 -0700 price=18.5 tid="183748">, <#Hashie::Rash amount=5 date=2011-06-14 11:27:00 -0700 price=18.42 tid="183749">]
     def trades
-      get('/code/data/getTrades.php').each{|t| t['date'] = Time.at(t['date'])}
+      get('/code/data/getTrades.php').each do |t|
+        t['date'] = Time.at(t['date'])
+      end
     end
-    
+
     # Fetch your balance
     # requires name and pass to be set
     #
@@ -61,7 +62,7 @@ module MtGox
     # @example
     #   MtGox.balance #=> <#Hashie::Rash btcs=3.7 usds=12> #this person has 3.7 bitcoins, and 12 dollars
     def balance
-      post('/code/getFunds.php',pass_params)
+      post('/code/getFunds.php', pass_params)
     end
 
     # Fetch your open bitcoin purchases
@@ -69,9 +70,13 @@ module MtGox
     #
     # @return [Array<Hashie::Rash>] an array of your open bids, sorted by price ascending each bid is a Hashie::Rash, with keys :amount, :dark, :date, :oid, :price, :status, and :type
     # @example
-    #   MtGox.buys[0,3] #=> [<#Hashie::Rash amount=0.73 dark="0" date="1307949196" oid="929284" price=2 status=:active type=2>, <#Hashie::Rash amount=0.36 dark="0" date="1307949201" oid="929288" price=4 status=:active type=2>, <#Hashie::Rash amount=0.24 dark="0" date="1307949212" oid="929292" price=6 status=:active type=2>]
+    #   MtGox.buys[0, 3] #=> [<#Hashie::Rash amount=0.73 dark="0" date="1307949196" oid="929284" price=2 status=:active type=2>, <#Hashie::Rash amount=0.36 dark="0" date="1307949201" oid="929288" price=4 status=:active type=2>, <#Hashie::Rash amount=0.24 dark="0" date="1307949212" oid="929292" price=6 status=:active type=2>]
     def buys
-      post('/code/getOrders.php',pass_params).orders.select {|o| o.type == ORDER_TYPES[:buy]}.each {|o| o.status = STATUS_TYPES.invert[o.status.to_i]}
+      post('/code/getOrders.php', pass_params)['orders'].select do |o|
+        o['type'] == ORDER_TYPES[:buy]
+      end.each do |o|
+        o['status'] = STATUS_TYPES.invert[o['status'].to_i]
+      end
     end
 
     # Fetch your open bitcoin sells
@@ -79,9 +84,13 @@ module MtGox
     #
     # @return [Array<Hashie::Rash>] an array of your open asks, sorted by price ascending each bid is a Hashie::Rash, with keys :amount, :dark, :date, :oid, :price, :status, and :type
     # @example
-    #   MtGox.sells[0,3] #=> [<#Hashie::Rash amount=0.1 dark="0" date="1307949384" oid="663465" price=24.92 status=nil type=1>, <#Hashie::Rash amount=0.12 dark="0" date="1307949391" oid="663468" price=25.65 status=nil type=1>, <#Hashie::Rash amount=0.15 dark="0" date="1307949396" oid="663470" price=26.38 status=nil type=1>]
+    #   MtGox.sells[0, 3] #=> [<#Hashie::Rash amount=0.1 dark="0" date="1307949384" oid="663465" price=24.92 status=nil type=1>, <#Hashie::Rash amount=0.12 dark="0" date="1307949391" oid="663468" price=25.65 status=nil type=1>, <#Hashie::Rash amount=0.15 dark="0" date="1307949396" oid="663470" price=26.38 status=nil type=1>]
     def sells
-      post('/code/getOrders.php',pass_params).orders.select {|o| o.type == ORDER_TYPES[:sell]}.each {|o| o.status = STATUS_TYPES.invert[o.status.to_i]}
+      post('/code/getOrders.php', pass_params)['orders'].select do |o|
+        o['type'] == ORDER_TYPES[:sell]
+      end.each do |o|
+        o['status'] = STATUS_TYPES.invert[o['status'].to_i]
+      end
     end
 
     # Fetch your open orders, both buys and sells, for network efficiency.
@@ -90,25 +99,27 @@ module MtGox
     # @return [<Hashie::Rash>] a Hashie::Rash with keys :buy and :sell, which contain arrays as described in #buys and #sells
     # @example
     #   o=MtGox.orders
-    #   o.buy[0,3]  #=> [<#Hashie::Rash amount=0.73 dark="0" date="1307949196" oid="929284" price=2 status=:active type=2>, <#Hashie::Rash amount=0.36 dark="0" date="1307949201" oid="929288" price=4 status=:active type=2>, <#Hashie::Rash amount=0.24 dark="0" date="1307949212" oid="929292" price=6 status=:active type=2>]
-    #   o.sell[0,3] #=> [<#Hashie::Rash amount=0.1 dark="0" date="1307949384" oid="663465" price=24.92 status=nil type=1>, <#Hashie::Rash amount=0.12 dark="0" date="1307949391" oid="663468" price=25.65 status=nil type=1>, <#Hashie::Rash amount=0.15 dark="0" date="1307949396" oid="663470" price=26.38 status=nil type=1>]
+    #   o.buy[0, 3]  #=> [<#Hashie::Rash amount=0.73 dark="0" date="1307949196" oid="929284" price=2 status=:active type=2>, <#Hashie::Rash amount=0.36 dark="0" date="1307949201" oid="929288" price=4 status=:active type=2>, <#Hashie::Rash amount=0.24 dark="0" date="1307949212" oid="929292" price=6 status=:active type=2>]
+    #   o.sell[0, 3] #=> [<#Hashie::Rash amount=0.1 dark="0" date="1307949384" oid="663465" price=24.92 status=nil type=1>, <#Hashie::Rash amount=0.12 dark="0" date="1307949391" oid="663468" price=25.65 status=nil type=1>, <#Hashie::Rash amount=0.15 dark="0" date="1307949396" oid="663470" price=26.38 status=nil type=1>]
     def orders
-      hash = post('/code/getOrders.php',pass_params).orders.each {|o| o.status = STATUS_TYPES.invert[o.status.to_i]; o.type=ORDER_TYPES.invert[o.type]}.group_by(&:type)
+      hash = post('/code/getOrders.php', pass_params)['orders'].each do |o|
+        o['status'] = STATUS_TYPES.invert[o['status'].to_i]
+        o['type'] = ORDER_TYPES.invert[o['type']]
+      end.group_by(&:type)
       Hashie::Rash.new(hash)
     end
-
 
     # places a buy request
     # requires name and pass to be set
     # TODO: usefully return something
     #
     # @param [Numeric] quantity the number of bitcoins to purchase
-    # @param [Numeric] price the price to buy them at 
+    # @param [Numeric] price the price to buy them at
     # @return [Array<Hashie::Rash>]
     # @example
     #   MtGox.buy 1.0, 25.0 #buy one BTC for 25 usd
-    def buy(quantity,price)
-      post('/code/buyBTC.php',pass_params.merge({:amount=>quantity,:price=>price}))
+    def buy(quantity, price)
+      post('/code/buyBTC.php', pass_params.merge({:amount => quantity, :price => price}))
       #.orders.select {|o| o.type == ORDER_TYPES[:sell]}.each {|o| o.status = STATUS_TYPES[o.status]}
     end
 
@@ -117,38 +128,38 @@ module MtGox
     # TODO: usefully return something
     #
     # @param [Numeric] quantity the number of bitcoins to sell
-    # @param [Numeric] price the price to sell them at 
+    # @param [Numeric] price the price to sell them at
     # @return [Array<Hashie::Rash>]
     # @example
     #   MtGox.sell 0.7, 26.0 #sell 0.7 BTC for 26 usd
-    def sell(quantity,price)
-      post('/code/sellBTC.php',pass_params.merge({:amount=>quantity,:price=>price}))
+    def sell(quantity, price)
+      post('/code/sellBTC.php', pass_params.merge({:amount => quantity, :price => price}))
       #.orders.select {|o| o.type == ORDER_TYPES[:sell]}.each {|o| o.status = STATUS_TYPES[o.status]}
     end
-    
+
     # cancels an open order
     # requires name and pass to be set
     # accepts as parameters as a hash-like object with "type" and "oid"
-    # type can either be a number (1,2) or a symbol (:buy,:sell)
+    # type can either be a number (1, 2) or a symbol (:buy, :sell)
     # TODO: usefully return something
     #
-    # @param [Hash] a hash-like object, with keys "oid": order ID of the transaction to cancel, and "type": the type of order to cancel. this can be a literal '1','2', or a symbolic :buy, :sell
+    # @param [Hash] a hash-like object, with keys "oid": order ID of the transaction to cancel, and "type": the type of order to cancel. this can be a literal '1', '2', or a symbolic :buy, :sell
     # @return Array<Hashie::Rash>
     # @example
     #   MtGox.cancel my_order
     #   MtGox.cancel {"oid" => "123", "type" => 2}
     def cancel(param)
-      order_params = param.select {|k,v| ["oid", "type"].include? k.to_s}
+      order_params = param.select {|k, v| ["oid", "type"].include? k.to_s}
       order_params["type"] = ORDER_TYPES[order_params["type"]]  || order_params["type"]
-      post('/code/cancelOrder.php',pass_params.merge(order_params))
+      post('/code/cancelOrder.php', pass_params.merge(order_params))
       #.orders.select {|o| o.type == ORDER_TYPES[:sell]}.each {|o| o.status = STATUS_TYPES[o.status]}
     end
 
-    
     private
+
     def pass_params
-      {"name"=>MtGox.name,"pass"=>MtGox.pass}
+      {:name => MtGox.name, :pass => MtGox.pass}
     end
-    
+
   end
 end
--- spec/mtgox/client_spec.rb
+++ spec/mtgox/client_spec.rb
@@ -24,35 +24,35 @@ describe MtGox::Client do
       stub_get('/code/data/getDepth.php').
         to_return(:status => 200, :body => fixture('depth.json'))
     end
-    
+
     describe '#asks' do
       it "should fetch open asks" do
         asks = @client.asks
         a_get('/code/data/getDepth.php').should have_been_made
         asks.last.should == [45, 593.28]
       end
-      
+
       it "should be sorted in price-ascending order" do
         asks = @client.asks
         asks.sort_by {|x| x[0]}.should == asks
       end
-      
+
     end
-    
+
     describe "#bids" do
       it "should fetch open bids" do
         bids = @client.bids
         a_get('/code/data/getDepth.php').should have_been_made
         bids.last.should == [19.1, 1]
       end
-      
+
       it "should be sorted in price-descending order" do
         bids = @client.bids
         bids.sort_by {|x| x[0]}.reverse.should == bids
       end
-      
+
     end
-    
+
     describe "#offers" do
       it "should fetch both bids and asks, making only 1 network request" do
         offers = @client.offers
@@ -78,19 +78,19 @@ describe MtGox::Client do
       trades.last.amount.should == 1.37
       trades.last.tid.should == "129606"
     end
-    
+
     it "should be sorted in chronological order" do
       trades = @client.trades
       trades.sort_by(&:date).should == trades
     end
   end
-  
+
   describe '#balance' do
     before do
       stub_post('/code/getFunds.php').
         to_return(:status => 200, :body => fixture('funds.json'))
     end
-    
+
     it "should fetch balance" do
       balance = @client.balance
       a_post("/code/getFunds.php").should have_been_made
@@ -98,13 +98,13 @@ describe MtGox::Client do
       balance.btcs.should == 22.0
     end
   end
-  
+
   describe "order methods" do
     before :each do
       stub_post('/code/getOrders.php').
         to_return(:status => 200, :body => fixture('orders.json'))
     end
-    
+
     describe "#buys" do
       it "should fetch orders" do
         buys = @client.buys
@@ -112,7 +112,7 @@ describe MtGox::Client do
         buys.last.price.should == 14
       end
     end
-    
+
     describe "#sells" do
       it "should fetch sells" do
         sells = @client.sells
@@ -120,7 +120,7 @@ describe MtGox::Client do
         sells.last.price.should == 29.3
       end
     end
-    
+
     describe "#orders" do
       it "should fetch both buys and sells, with only one call" do
         orders = @client.orders
@@ -129,18 +129,20 @@ describe MtGox::Client do
         orders.sell.last.price.should == 29.3
       end
     end
-    
+
   end
-  
+
   describe "#buy" do
     before do
       stub_post('/code/buyBTC.php').
         to_return(:status => 200, :body => fixture('buy.json'))
     end
-    
+
     it "should place a bid" do
-      @client.buy(0.88,0.89)
-      a_post("/code/buyBTC.php").with(:body=> {"name"=>"my_name","pass"=>"my_password","amount"=>"0.88","price"=>"0.89"}).should have_been_made
+      @client.buy(0.88, 0.89)
+      a_post("/code/buyBTC.php").
+        with(:body => {"name" => "my_name", "pass" => "my_password", "amount" => "0.88", "price" => "0.89"}).
+        should have_been_made
     end
   end
 
@@ -149,13 +151,15 @@ describe MtGox::Client do
       stub_post('/code/sellBTC.php').
         to_return(:status => 200, :body => fixture('sell.json'))
     end
-    
+
     it "should place an ask" do
-      @client.sell(0.88,89.0)
-      a_post("/code/sellBTC.php").with(:body=> {"name"=>"my_name","pass"=>"my_password","amount"=>"0.88","price"=>"89.0"}).should have_been_made
+      @client.sell(0.88, 89.0)
+      a_post("/code/sellBTC.php").
+        with(:body => {"name" => "my_name", "pass" => "my_password", "amount" => "0.88", "price" => "89.0"}).
+        should have_been_made
     end
   end
-  
+
   describe "cancel" do
     before do
       stub_post('/code/getOrders.php').
@@ -163,12 +167,14 @@ describe MtGox::Client do
       stub_post('/code/cancelOrder.php').
         to_return(:status => 200, :body => fixture('cancel.json'))
     end
-    
+
     it "should cancel an order" do
       buy = @client.buys.last
       @client.cancel(buy)
-      a_post('/code/cancelOrder.php').with(:body=> {"name"=>"my_name","pass"=>"my_password","oid"=>"929308","type"=>"2"}).should have_been_made
+      a_post('/code/cancelOrder.php').
+        with(:body => {"name" => "my_name", "pass" => "my_password", "oid" => "929308", "type" => "2"}).
+        should have_been_made
     end
   end
-  
+
 end
