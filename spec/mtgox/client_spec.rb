0cad8cd0d41d4a15d4fd003eebb12ebc79e63f70
T S <YenTheFirst@gmail.com>
2011-06-16 11:47:59 -0700

    added #offers to spec, and added specs for ordering conditions I added in the YARD docs

--- spec/mtgox/client_spec.rb
+++ spec/mtgox/client_spec.rb
@@ -18,30 +18,49 @@ describe MtGox::Client do
     end
   end
 
-  describe '#asks' do
-    before do
+  describe 'depth methods' do
+    before :each do
       stub_get('/code/data/getDepth.php').
         to_return(:status => 200, :body => fixture('depth.json'))
     end
-
-    it "should fetch open asks" do
-      asks = @client.asks
-      a_get('/code/data/getDepth.php').should have_been_made
-      asks.last.should == [45, 593.28]
+    
+    describe '#asks' do
+      it "should fetch open asks" do
+        asks = @client.asks
+        a_get('/code/data/getDepth.php').should have_been_made
+        asks.last.should == [45, 593.28]
+      end
+      
+      it "should be sorted in price-ascending order" do
+        asks = @client.asks
+        asks.sort_by {|x| x[0]}.should == asks
+      end
+      
     end
-  end
-
-  describe '#bids' do
-    before do
-      stub_get('/code/data/getDepth.php').
-        to_return(:status => 200, :body => fixture('depth.json'))
+    
+    describe "#bids" do
+      it "should fetch open bids" do
+        bids = @client.bids
+        a_get('/code/data/getDepth.php').should have_been_made
+        bids.last.should == [19.1, 1]
+      end
+      
+      it "should be sorted in price-descending order" do
+        bids = @client.bids
+        bids.sort_by {|x| x[0]}.reverse.should == bids
+      end
+      
+    end
+    
+    describe "#offers" do
+      it "should fetch both bids and asks, making only 1 network request" do
+        offers = @client.offers
+        a_get('/code/data/getDepth.php').should have_been_made.once
+        offers.asks.last.should == [45, 593.28]
+        offers.bids.last.should == [19.1, 1]
+      end
     end
 
-    it "should fetch open bids" do
-      bids = @client.bids
-      a_get('/code/data/getDepth.php').should have_been_made
-      bids.last.should == [19.1, 1]
-    end
   end
 
   describe '#trades' do
@@ -57,6 +76,11 @@ describe MtGox::Client do
       trades.last.price.should == 26.6099
       trades.last.amount.should == 1.37
       trades.last.tid.should == "129606"
+    end
+    
+    it "should be sorted in chronological order" do
+      trades = @client.trades
+      trades.sort_by(&:date).should == trades
     end
   end
 end
