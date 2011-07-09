29f61b545e3c3cd47efd1512c06d2859b318a289
Erik Michaels-Ober <sferik@gmail.com>
2011-07-09 14:17:28 -0700

    More explicit requirements

--- lib/faraday/response/raise_mtgox_error.rb
+++ lib/faraday/response/raise_mtgox_error.rb
@@ -9,4 +9,3 @@ module Faraday
     end
   end
 end
-
--- lib/mtgox/connection.rb
+++ lib/mtgox/connection.rb
@@ -1,4 +1,8 @@
 require 'faraday'
+require 'faraday/request/url_encoded'
+require 'faraday/response/raise_error'
+require 'faraday/response/rashify'
+require 'faraday/response/parse_json'
 require 'faraday/response/raise_mtgox_error'
 require 'faraday_middleware'
 require 'mtgox/version'
