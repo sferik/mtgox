775a4ca2a61a5de62d5c7c243f0cc40397e5d5de
Kurt Thams <thams@thams.com>
2013-03-30 12:44:35 -0700

    MtGox now requires API calls to data.mtgox.com, declines calls to mtgox.com with a HTTP 403 response

--- spec/helper.rb
+++ spec/helper.rb
@@ -15,19 +15,19 @@ require 'webmock/rspec'
 WebMock.disable_net_connect!(:allow => 'coveralls.io')
 
 def a_get(path)
-  a_request(:get, 'https://mtgox.com' + path)
+  a_request(:get, 'https://data.mtgox.com' + path)
 end
 
 def stub_get(path)
-  stub_request(:get, 'https://mtgox.com' + path)
+  stub_request(:get, 'https://data.mtgox.com' + path)
 end
 
 def a_post(path)
-  a_request(:post, 'https://mtgox.com' + path)
+  a_request(:post, 'https://data.mtgox.com' + path)
 end
 
 def stub_post(path)
-  stub_request(:post, 'https://mtgox.com' + path)
+  stub_request(:post, 'https://data.mtgox.com' + path)
 end
 
 def fixture_path
