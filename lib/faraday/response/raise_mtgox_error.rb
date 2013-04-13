1c078aca88b15889001422e3ae9c12762a6fea55
Julien 'Lta' BALLET <elthariel@gmail.com>
2013-04-13 17:48:46 +0200

    Removes a few #puts laying around

--- lib/faraday/response/raise_mtgox_error.rb
+++ lib/faraday/response/raise_mtgox_error.rb
@@ -4,8 +4,6 @@ require 'json'
 module Faraday
   class Response::RaiseMtGoxError < Response::Middleware
     def on_complete(env)
-      puts "#####"
-      puts env[:status]
       if 200 == env[:status] && 'MySQL error, please retry later' == env[:body]
         raise MtGox::MysqlError, "MySQL error, please retry later"
       elsif 403 == env[:status] && MultiJson.load(env[:body])["result"] == "error"
