62593cdf8a271a5adae2a3888b1156d7d0db5d51
Julien 'Lta' BALLET <elthariel@gmail.com>
2013-04-15 01:39:47 +0200

    Adds a test for MtGox::UnauthorizedError handler

--- lib/faraday/response/raise_mtgox_error.rb
+++ lib/faraday/response/raise_mtgox_error.rb
@@ -7,7 +7,7 @@ module Faraday
       if 200 == env[:status] && 'MySQL error, please retry later' == env[:body]
         raise MtGox::MysqlError, "MySQL error, please retry later"
       elsif 403 == env[:status] && MultiJson.load(env[:body])["result"] == "error"
-        raise MtGox::Unauthorized, MultiJson.load(env[:body])["error"]
+        raise MtGox::UnauthorizedError, MultiJson.load(env[:body])["error"]
       elsif 404 != env[:status] && MultiJson.load(env[:body])["result"] == "error"
         raise MtGox::Error, MultiJson.load(env[:body])["error"]
       end
--- spec/faraday/response_spec.rb
+++ spec/faraday/response_spec.rb
@@ -8,9 +8,7 @@ describe Faraday::Response do
     end
 
     it "should raise MtGox::MysqlError" do
-      lambda do
-        MtGox.trades
-      end.should raise_error(MtGox::MysqlError)
+      expect { MtGox.trades }.to raise_error(MtGox::MysqlError)
     end
   end
 
@@ -21,9 +19,23 @@ describe Faraday::Response do
     end
 
     it "should raise MtGox::Error" do
-      lambda do
-        MtGox.trades
-      end.should raise_error(MtGox::Error)
+      expect { MtGox.trades }.to raise_error(MtGox::Error)
     end
+
+    describe "UnauthorizedError" do
+      before do
+        @client = MtGox::Client.new
+        body = test_body({"amount_int" => "100000000", "address" => "1KxSo9bGBfPVFEtWNLpnUK1bfLNNT4q31L"})
+        stub_post('/api/1/generic/bitcoin/send_simple').
+          with(body: body, headers: test_headers(body)).
+          to_return(status: 403, body: fixture('error.json'))
+      end
+
+      it "should raise MtGox::UnauthorizedError" do
+        expect { @client.withdraw!(1.0, "1KxSo9bGBfPVFEtWNLpnUK1bfLNNT4q31L") }.
+          to raise_error(MtGox::UnauthorizedError)
+      end
+    end
+
   end
 end
