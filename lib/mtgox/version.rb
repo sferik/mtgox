6ca280081c5f870930c59ae429cafa22cb8490cf
Erik Michaels-Ober <sferik@gmail.com>
2013-04-15 03:35:19 -0700

    Bump version to 0.9.0

--- lib/mtgox/version.rb
+++ lib/mtgox/version.rb
@@ -8,12 +8,12 @@ module MtGox
 
     # @return [Integer]
     def self.minor
-      8
+      9
     end
 
     # @return [Integer]
     def self.patch
-      2
+      0
     end
 
     # @return [String, NilClass]
